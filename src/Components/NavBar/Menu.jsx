ad6dea68c2bf43bcdd9c54d08268d0b20f301706
PrajwalHs2609 <prajwalhs4india@gmail.com>
2024-09-13 15:07:49 +0530

    second commit

--- src/Components/NavBar/Menu.jsx
+++ src/Components/NavBar/Menu.jsx
@@ -341,7 +341,7 @@ const Menu = () => {
             </div>
           </li>
           <li>
-            <Link to={"/mea-attestation-apostille-services"}>ME</Link>
+            <Link to={"/mea-attestation-apostille-services"}>MEA</Link>
             {/* <FontAwesomeIcon
               icon={faAngleDown}
               className="menuIcon"
